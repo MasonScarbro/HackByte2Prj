690a932f0e46a89ec1170e4bfd4f960255abee30
sohana929@gmail.com <sohana929@gmail.com>
2023-06-30 17:59:23 -0500

    finished carbon calc

--- carbonweb/carboncalc/urls.py
+++ carbonweb/carboncalc/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('result/', views.result, name='result'),
+    path('result/aboveaverage', views.result, name='aboavg'),
+    path('result/aboveaverage', views.result, name='belavg')
 ]
--- carbonweb/carboncalc/views.py
+++ carbonweb/carboncalc/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import pandas as pd
 
 # Create your views here.
 
@@ -10,8 +11,13 @@ def result(request):
     num2 = int(request.GET.get('number2'))
     num3 = int(request.GET.get('number3'))
     num4 = int(request.GET.get('number4'))
+    num5 = int(request.GET.get('number5'))
 
-    
     if request.GET.get('calculate') == "":
-        ans = num1 + num2 + num3 + num4
-    return render(request, 'result.html', {'ans': ans})
+        ans = (num1 * 105) + (num2 * 105) + (num3 * 113) + (num4 * .79) + (num5 * 2700)
+    if ans > 45000:
+        return render(request, 'aboavg.html', {'ans': ans})
+    if ans < 25000:
+        return render(request, 'belavg.html', {'ans': ans})
+    else:
+        return render(request, 'result.html', {'ans': ans})
